27e555795324ed9c4b95e535530818eb0b1b9f51
Reymar Enteria <reymar.developer@gmail.com>
2023-01-12 00:46:03 +0800

    Completed

--- src/App/index.js
+++ src/App/index.js
@@ -1,12 +1,73 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+import Films from '../Films';
 import './style.css';
 
+const filmsEndpointURL = "https://app.codescreen.com/api/assessments/films"
+const apiToken = "8c5996d5-fb89-46c9-8821-7063cfbc18b1"
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      films: [],
+      value: ''
+    }
+  }
+
+  getDirectorFilms = (e) => {
+    e.preventDefault();
+
+    axios.get(filmsEndpointURL, {
+      headers: {
+        Authorization: `Bearer ${apiToken}`
+      },
+      params: {
+        directorName: this.state.value
+      }
+    }).then(response => {
+      const films = response.data;
+
+      this.setState({
+        films
+      });
+    }).catch(error => {
+      console.log(error);
+    });
+  }
+
+  handleChange = (event) => { 
+    this.setState({value: event.target.value});
+  }
+
+
 
   render() {
     return (
       <div>
         <p className="films-analysis-service">Films Analysis Service </p>
+
+        <form id="input-form" onSubmit={this.getDirectorFilms} className='form'>
+          <input 
+            id="input-box" 
+            className="enter-director-name" 
+            placeholder='Enter director name'
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+          <button
+            type='submit'
+            className="submit-button"
+          >
+            <p className="submit-button-text">Submit</p>
+          </button>
+        </form>
+
+        <Films 
+          films={this.state.films}
+        />
       </div>
     );
   }
--- src/Films/index.js
+++ src/Films/index.js
@@ -1,23 +1,56 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 import './style.css';
 
-const axios = require('axios');
-
-const filmsEndpointURL = "https://app.codescreen.com/api/assessments/films";
-
-//Your API token. This is needed to successfully authenticate when calling the films endpoint. 
-//This needs to be added to the Authorization header (using the Bearer authentication scheme) in the request you send to the films endpoint.
-const apiToken = "8c5996d5-fb89-46c9-8821-7063cfbc18b1"
-
 export default class Films extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      cards: []
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.films !== this.props.films) {
+      this.setState({
+        cards: [
+          {
+            id: 'best-rated-film',
+            label: "Best rated film",
+            value: this.getBestRatedFilm(nextProps.films)
+          },
+          {
+            id: 'longest-film',
+            label: "Longest film duration",
+            value: this.getLongestFilm(nextProps.films)
+          },
+          {
+            id: 'average-rating',
+            label: "Average rating",
+            value: this.getAverageRating(nextProps.films)
+          },
+          {
+            id: 'shortest-days-between-releases',
+            label: "Shortest days between releases",
+            value: this.getShortestNumberOfDaysBetweenFilmReleases(nextProps.films)
+          }
+        ]
+      });
+    }
+  }
 
   /**
     * Retrieves the name of the best rated film from the given list of films.
     * If the given list of films is empty, this method should return "N/A".
   */
   getBestRatedFilm(films) {
-    //TODO Implement
+    if (films.length === 0 ) return "N/A";
+
+    const bestRating = films.sort((a, b) => b.rating - a.rating);
+    return bestRating[0].name;
   }
+  
 
   /**
     * Retrieves the length of the film which has the longest running time from the given list of films.
@@ -27,7 +60,10 @@ export default class Films extends Component {
     * For example, if the duration of the longest film is 120, this function should return "120 mins".
   */
   getLongestFilm(films) {
-    //TODO Implement
+    if (films.length === 0 ) return "N/A";
+
+    const longestFilm = films.sort((a, b) => b.length - a.length);
+    return `${longestFilm[0].length} mins`;
   }
 
   /**
@@ -35,7 +71,10 @@ export default class Films extends Component {
     * If the given list of films is empty, this method should return 0.
   */
   getAverageRating(films) {
-    //TODO Implement
+    if (films.length === 0 ) return 0;
+
+    const totalRating = films.reduce((acc, film) => acc + film.rating, 0);
+    return (totalRating / films.length).toFixed(1);
   }
 
   /**
@@ -73,7 +112,36 @@ export default class Films extends Component {
     * then this method should return 147, as Prestige was released 147 days after Batman Begins.
   */
   getShortestNumberOfDaysBetweenFilmReleases(films) {
-    //TODO Implement
+    if (films.length === 0 ) return "N/A";
+    if (films.length === 1 ) return 0;
+
+    const sortedFilms = films.sort((a, b) => new Date(a.releaseDate) - new Date(b.releaseDate));
+
+    const daysBetweenReleases = sortedFilms.map((film, index) => {
+      if (index === 0) return null;
+
+      const days = moment(film.releaseDate).diff(sortedFilms[index - 1].releaseDate, 'days');
+
+      return days;
+    });
+
+    return Math.min(...daysBetweenReleases.filter(Boolean));
   }
 
+  render() {
+    const {
+      cards
+    } = this.state;
+
+    return (
+      <div className='stats-boxes'>
+        {cards.length > 0 && cards.map(card => (
+        <div className='stats-box stats-box-right' key={card.id}>
+          <p className='stats-box-heading'>{card.label}</p>
+          <p className='stats-box-info'>{card.value}</p>
+        </div>
+        ))}
+      </div>
+    );
+  }
 }
